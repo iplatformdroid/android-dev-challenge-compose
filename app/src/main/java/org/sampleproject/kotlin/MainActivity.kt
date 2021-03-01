d33e1ccbff7d6899b9329dbb3f904c511dc764b1
iplatformdroid <iplatformdroid@gmail.com>
2021-03-01 22:23:39 +0900

    Removed auto import.

--- app/src/main/java/org/sampleproject/kotlin/MainActivity.kt
+++ app/src/main/java/org/sampleproject/kotlin/MainActivity.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.sampleproject.kotlin
 
 import android.os.Bundle
@@ -6,12 +21,18 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.material.Card
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -19,13 +40,16 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavType
-import androidx.navigation.compose.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
 import com.example.androiddevchallenge.R
 
 val petList = listOf(
     Pet(R.drawable.cat1_icon, R.drawable.cat1, "Mike", "Japan"),
-    Pet(R.drawable.cat2_icon, R.drawable.cat2,"Nyan", "China"),
-    Pet(R.drawable.cat3_icon, R.drawable.cat3,"Yam", "Korea")
+    Pet(R.drawable.cat2_icon, R.drawable.cat2, "Nyan", "China"),
+    Pet(R.drawable.cat3_icon, R.drawable.cat3, "Yam", "Korea")
 )
 
 class MainActivity : AppCompatActivity() {
@@ -39,19 +63,20 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun App(){
+fun App() {
     val navController = rememberNavController()
     NavHost(
         navController = navController,
-        startDestination = "home"){
+        startDestination = "home"
+    ) {
         composable("home") {
-                   MainScreen(navController, petList)
-            }
+            MainScreen(navController, petList)
+        }
         composable(
             "detail/{petId}",
             arguments = listOf(navArgument("petId") { type = NavType.StringType })
         ) {
-                backStackEntry ->
+            backStackEntry ->
             DetailScreen(navController, backStackEntry.arguments?.getString("petId"))
         }
     }
@@ -65,14 +90,16 @@ fun MainScreen(navController: NavController, petList: List<Pet>) {
             modifier = Modifier.padding(16.dp)
         ) {
 
-            LazyColumn (
+            LazyColumn(
                 contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
             ) {
 
                 items(petList.size) { index ->
                     PetCard(navController, index.toString(), petList.get(index))
-                    Spacer(modifier = Modifier
-                        .height(8.dp),)
+                    Spacer(
+                        modifier = Modifier
+                            .height(8.dp),
+                    )
                 }
             }
         }
@@ -85,22 +112,23 @@ fun PetCard(navController: NavController, id: String, pet: Pet) {
         Log.d("@@@Main", "name=${pet.name}")
         navController.navigate("detail/$id")
     }
-    Card{
-    Row(
-        Modifier
-            .clickable(onClick = onClick),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Image(
-            painter = painterResource(pet.iconId),
-            contentDescription = null,
-            modifier = Modifier
-                .height(64.dp),)
-        Column {
-            Text(pet.name, style = typography.h6)
-            Text(pet.detail)
+    Card {
+        Row(
+            Modifier
+                .clickable(onClick = onClick),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(pet.iconId),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(64.dp),
+            )
+            Column {
+                Text(pet.name, style = typography.h6)
+                Text(pet.detail)
+            }
         }
-    }
     }
 }
 
@@ -129,8 +157,8 @@ fun DetailScreen(navController: NavController, petId: String?) {
     }
 }
 
-//@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF, showSystemUi = true)
-//@Composable
+// @Preview(showBackground = true, backgroundColor = 0xFFFFFFFF, showSystemUi = true)
+// @Composable
 fun DefaultPreview() {
 //    MainScreen(null, petList)
 }
