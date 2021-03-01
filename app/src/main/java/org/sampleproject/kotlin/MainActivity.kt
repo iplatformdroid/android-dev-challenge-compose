0a3feb4de88c09feaec2a0d937855df14162b644
iplatformdroid <iplatformdroid@gmail.com>
2021-03-01 23:04:12 +0900

    Modified lack of import.

--- app/src/main/java/org/sampleproject/kotlin/MainActivity.kt
+++ app/src/main/java/org/sampleproject/kotlin/MainActivity.kt
@@ -43,6 +43,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.navigate
 import androidx.navigation.compose.rememberNavController
 import com.example.androiddevchallenge.R
 
